29fdef0d0324f504d8b254fbc89d41eb1e8cd096
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-05-26 19:29:50 +0530

    Add files via upload

---
+++ program33.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    //Filter
+    if(iNo < 0)
+    {
+        printf("Invalid Input\n");
+        return;
+    }
+    
+    for(int iCnt=0;iCnt<=iNo ;iCnt++)
+    {
+        printf("%d \n",iCnt);
+    }
+
+}
+int main()
+{
+    int iValue=0;
+
+    printf("Enter the frequency: ");
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+    return 0;
+}
