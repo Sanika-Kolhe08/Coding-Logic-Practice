ad4fa1efaa87f1f26ebf2a3ee277c8eb465e1e90
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-05-27 18:58:31 +0530

    Add files via upload

---
+++ program37.java
@@ -0,0 +1,9 @@
+public class program37 {
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+        for(iCnt = 1; iCnt <= 10 ; iCnt++ ){
+            System.out.println(iCnt);
+        }
+    }
+}
